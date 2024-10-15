6c62e5bb486d44f7c630b06a40b0fc06e80b82d8
Tchana <valdotnv@gmail.com>
2024-10-15 13:00:59 +0100

    implement bottom navigation

--- lib/features/authentication/screens/login/widgets/shopingly_login_form.dart
+++ lib/features/authentication/screens/login/widgets/shopingly_login_form.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:shopingly/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:shopingly/features/authentication/screens/signup/signup.dart';
+import 'package:shopingly/navigation_menu.dart';
 import 'package:shopingly/utils/constants/sizes.dart';
 import 'package:shopingly/utils/constants/text_strings.dart';
 
@@ -58,7 +59,7 @@ class ShopinglyLoginForm extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () => Get.to(()=> NavigationMenu()),
                 child: Text(ShopinglyTexts.signIn),
               ),
             ),
